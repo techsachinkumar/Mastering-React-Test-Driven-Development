a0a9c887f82a6b4030a4aa36833c57475fba52fa
Daniel Irvine <daniel@danielirvine.com>
2019-01-15 10:23:33 +0000

    Add reset button

--- spec-logo/src/MenuButtons.js
+++ spec-logo/src/MenuButtons.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { useDispatch, useMappedState } from 'redux-react-hook';
+import { initialState } from './parser';
+const { useCallback } = React;
 
 export const MenuButtons = () => {
-  return null;
+  const mapState = useCallback(({ script }) => ({ script }), []);
+  const { script } = useMappedState(mapState);
+
+  const dispatch = useDispatch();
+
+  const reset = useCallback(() => dispatch({ type: 'RESET' }));
+  const canReset = script !== initialState;
+
+  return (
+    <React.Fragment>
+      <button onClick={reset} disabled={!canReset}>Reset</button>
+    </React.Fragment>
+  );
 };
--- spec-logo/test/MenuButtons.test.js
+++ spec-logo/test/MenuButtons.test.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { StoreContext } from 'redux-react-hook';
+import { expectRedux, storeSpy } from 'expect-redux';
+import { configureStore } from '../src/store';
 import { MenuButtons } from '../src/MenuButtons';
 
 describe('MenuButtons', () => {
   let wrapper;
+  let store;
 
-  it('renders', () => {
-    wrapper = mount(<MenuButtons />);
-    expect(wrapper.find('MenuButtons').exists()).toBeTruthy();
+  beforeEach(() => {
+    store = configureStore([storeSpy]);
+  });
+
+  function mountWithStore(component) {
+    return mount(<StoreContext.Provider value={store}>{component}</StoreContext.Provider>);
+  }
+
+  function button(text) {
+    return wrapper.find('button').findWhere(b => b.text() === text);
+  }
+
+  describe('reset button', () => {
+    it('renders', () => {
+      wrapper = mountWithStore(<MenuButtons />);
+      expect(button('Reset').exists()).toBeTruthy();
+    });
+
+    it('is disabled initially', () => {
+      wrapper = mountWithStore(<MenuButtons />);
+      expect(button('Reset').prop('disabled')).toBeTruthy();
+    });
+
+    it('is enabled once a state change occurs', () => {
+      store.dispatch({ type: 'SUBMIT_EDIT_LINE', text: 'forward 10\n' });
+      wrapper = mountWithStore(<MenuButtons />);
+      expect(button('Reset').prop('disabled')).toBeFalsy();
+    });
+
+    it('dispatches an action of RESET when clicked', () => {
+      store.dispatch({ type: 'SUBMIT_EDIT_LINE', text: 'forward 10\n' });
+      wrapper = mountWithStore(<MenuButtons />);
+      button('Reset').simulate('click');
+      expectRedux(store)
+        .toDispatchAnAction()
+        .matching({ type: 'RESET' });
+    });
   });
 });
