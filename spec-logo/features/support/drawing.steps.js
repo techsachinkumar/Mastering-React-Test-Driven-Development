4a0c4597c9a9cf7fdeaef28096babbca6c898bac
Daniel Irvine <daniel@danielirvine.com>
2019-01-24 14:35:43 +0000

    Add step definitions for sharing features

--- spec-logo/features/support/drawing.steps.js
+++ spec-logo/features/support/drawing.steps.js
@@ -1,6 +1,7 @@
 import { Given, When, Then } from 'cucumber';
 import expect from 'expect';
 import { browseToPageFor, appPage } from './browser';
+import { checkLinesFromDataTable } from './svg';
 
 Given('the user navigated to the application page', async function () {
   await browseToPageFor.call(this, 'user', appPage);
@@ -10,22 +11,7 @@ When('the user enters the following instructions at the prompt:', async function
   for (let instruction of dataTable.raw()) {
     await this.getPage('user').type('textarea', `${instruction}\n`);
   }
+  await this.getPage('user').waitFor(3000);
 });
 
-Then('these lines should have been drawn in order:', async function (dataTable) {
-  await this.getPage('user').waitFor(3000);
-  const lines = await this.getPage('user').$$eval('line', lines =>
-    lines.map(line => { return {
-      x1: parseFloat(line.getAttribute('x1')),
-      y1: parseFloat(line.getAttribute('y1')),
-      x2: parseFloat(line.getAttribute('x2')),
-      y2: parseFloat(line.getAttribute('y2'))
-    }; })
-  );
-  for (let i = 0; i < lines.length; ++i) {
-    expect(lines[i].x1).toBeCloseTo(parseInt(dataTable.hashes()[i].x1));
-    expect(lines[i].y1).toBeCloseTo(parseInt(dataTable.hashes()[i].y1));
-    expect(lines[i].x2).toBeCloseTo(parseInt(dataTable.hashes()[i].x2));
-    expect(lines[i].y2).toBeCloseTo(parseInt(dataTable.hashes()[i].y2));
-  }
-});
+Then('these lines should have been drawn in order:', checkLinesFromDataTable('user'));
--- spec-logo/features/support/sharing.steps.js
+++ spec-logo/features/support/sharing.steps.js
@@ -1,22 +1,15 @@
 import { Given, When, Then } from 'cucumber';
+import { browseToPageFor, appPage } from './browser';
+import { checkLinesFromDataTable } from './svg';
+import { calculateTurtleXYFromPoints, calculateTurtleAngleFromTransform } from './turtle';
 import expect from 'expect';
-import puppeteer from 'puppeteer';
-
-const port = process.env.PORT || 3000;
-export const appPage = `http://localhost:${port}/index.html`;
-
-export async function browseToPageFor(role, url) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  this.setPage(role, page);
-}
 
 Given('the presenter navigated to the application page', async function () {
   await browseToPageFor.call(this, 'presenter', appPage);
 });
 
 Given('the presenter clicked the button {string}', async function (buttonId) {
+  await this.getPage('presenter').waitForSelector(`button#${buttonId}`);
   await this.getPage('presenter').click(`button#${buttonId}`);
 });
 
@@ -31,3 +24,54 @@ Then('the observer should see a message saying {string}', async function (messag
   const pageText = await this.getPage('observer').$eval('body', e => e.outerHTML);
   expect(pageText).toContain(message);
 });
+
+When('the presenter entered the following instructions at the prompt:', async function (dataTable) {
+  for (let instruction of dataTable.raw()) {
+    await this.getPage('presenter').type('textarea', `${instruction}\n`);
+  }
+  await this.getPage('presenter').waitFor(3500);
+});
+
+When('the presenter clicks the button {string}', async function (buttonId) {
+  await this.getPage('presenter').waitForSelector(`button#${buttonId}`);
+  await this.getPage('presenter').click(`button#${buttonId}`);
+});
+
+Then('the observer should see no lines', async function () {
+  const numLines = await this.getPage('observer').$$eval('line', lines => lines.length);
+  expect(numLines).toEqual(0);
+});
+
+Then('the presenter should see no lines', async function () {
+  const numLines = await this.getPage('presenter').$$eval('line', lines => lines.length);
+  expect(numLines).toEqual(0);
+});
+
+Then('the observer should see the turtle at x = {int}, y = {int}, angle = {int}', async function (expectedX, expectedY, expectedAngle) {
+  await this.getPage('observer').waitFor(4000);
+  const turtle = await this.getPage('observer').$eval('polygon', polygon => ({
+    points: polygon.getAttribute('points'),
+    transform: polygon.getAttribute('transform')
+  }));
+  const position = calculateTurtleXYFromPoints(turtle.points);
+  const angle = calculateTurtleAngleFromTransform(turtle.transform);
+  expect(position.x).toBeCloseTo(expectedX);
+  expect(position.y).toBeCloseTo(expectedY);
+  expect(angle).toBeCloseTo(expectedAngle);
+});
+
+Then('the presenter should see the turtle at x = {int}, y = {int}, angle = {int}', async function (expectedX, expectedY, expectedAngle) {
+  await this.getPage('presenter').waitFor(4000);
+  const turtle = await this.getPage('presenter').$eval('polygon', polygon => ({
+    points: polygon.getAttribute('points'),
+    transform: polygon.getAttribute('transform')
+  }));
+  const position = calculateTurtleXYFromPoints(turtle.points);
+  const angle = calculateTurtleAngleFromTransform(turtle.transform);
+  expect(position.x).toBeCloseTo(expectedX);
+  expect(position.y).toBeCloseTo(expectedY);
+  expect(angle).toBeCloseTo(expectedAngle);
+});
+
+Then('these lines should have been drawn for the presenter:', checkLinesFromDataTable('presenter'));
+Then('these lines should have been drawn for the observer:', checkLinesFromDataTable('observer'));
